294e7829726352e90bf5226542b2f667b2f71c75
fanm1282-dotcom <fanm1282@gmail.com>
2026-05-26 02:27:55 +0700

    Create Memory.js

---
+++ models/Memory.js
@@ -0,0 +1,27 @@
+const mongoose =
+  require('mongoose');
+
+const memorySchema =
+  new mongoose.Schema({
+
+    userId: String,
+
+    question: String,
+
+    answer: String,
+
+    createdAt: {
+
+      type: Date,
+
+      default: Date.now
+
+    }
+
+});
+
+module.exports =
+  mongoose.model(
+    'Memory',
+    memorySchema
+  );
